5fea7bb87f2d586bfb3f889f83661e466d563ce0
Sergey Evdokimov <sergey.evdokimov@jetbrains.com>
2014-03-25 19:02:47 +0400

    Add IntelliJ plugin page, extract menu to menu.js

---
+++ GWT/src/main/webapp/js/menu.js
@@ -0,0 +1,23 @@
+document.write("<div id='tBar'>" +
+               "    <div class='out' style='margin-left:30px'>" +
+               "        <a href='./'>" +
+               "            <div>Online regex tester</div>" +
+               "        </a>" +
+               "    </div>" +
+               "    <div class='out'>" +
+               "        <a href='eclipsePlugin.html'>" +
+               "            <div>Eclipse Plugin</div>" +
+               "        </a>" +
+               "    </div>" +
+               "    <div class='out'>" +
+               "        <a href='ideaPlugin.html'>" +
+               "            <div class='actLink'>IntelliJ Plugin</div>" +
+               "        </a>" +
+               "    </div>" +
+               "    <div class='out'>" +
+               "        <a href='examples.html'>" +
+               "            <div>Regex Examples</div>" +
+               "        </a>" +
+               "    </div>" +
+               "    <div style='clear:both'></div>" +
+               "</div>")
