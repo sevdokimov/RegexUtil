7e87ad3ef9868f1ff9f93153dc1789200446f9ba
Sergey Evdokimov <sergey.evdokimov@jetbrains.com>
2014-03-30 01:58:33 +0400

    Add tests

--- GWT/src/test/html/testUtil.js
+++ GWT/src/test/html/testUtil.js
@@ -1,8 +1,34 @@
-function testRegexHighlighting(mode, text, tokens) {
+function testRegexHighlighting(text, tokens) {
   testHighlighting("ace/mode/regexp", text, tokens)
 }
 
 function testHighlighting(mode, text, tokens) {
-  var DebugTokenizer = require("ace/tokenizer_dev").Tokenizer;
-  
+    var Mode = require(mode).Mode;
+
+    var m = new Mode()
+
+  var Tokenizer = require("ace/tokenizer").Tokenizer;
+    var tokenizer = new Tokenizer(new m.HighlightRules().$rules);
+
+    var t = tokenizer.getLineTokens(text).tokens
+
+    var sucess = tokens.length == t.length
+
+    if (sucess) {
+        for (var i = 0; i < tokens.length; i++) {
+            var tmpl = tokens[i]
+            var idx = tmpl.indexOf('#')
+
+            if (idx == -1) {
+                sucess = (t[i].type == tmpl || t[i].value == tmpl);
+            }
+            else {
+                sucess = (t[i].type = tmpl.substr())
+            }
+
+            if (!sucess) break
+        }
+    }
+
+    assert(z)
 }
