bce9f53bb2095862498bdb17c0e85a46f0c310b5
Sergey Evdokimov <sergey.evdokimov@jetbrains.com>
2014-03-29 17:10:33 +0400

    Use "conflict" ace instead of "noconflict"

--- GWT/src/main/webapp/js/regexp/regexp_highlight_rules.js
+++ GWT/src/main/webapp/js/regexp/regexp_highlight_rules.js
@@ -1,112 +1,112 @@
-ace.define('ace/mode/regexp_highlight_rules', ['require', 'exports', 'module' , 'ace/lib/oop', 'ace/mode/text_highlight_rules'],
-           function (require, exports, module) {
+define('ace/mode/regexp_highlight_rules', ['require', 'exports', 'module' , 'ace/lib/oop', 'ace/mode/text_highlight_rules'],
+       function (require, exports, module) {
 
 
-             var oop = require("../lib/oop");
-             var TextHighlightRules = require("./text_highlight_rules").TextHighlightRules;
+         var oop = require("../lib/oop");
+         var TextHighlightRules = require("./text_highlight_rules").TextHighlightRules;
 
-             var oneSymbolMap = {
-               '(': "openBracket",
-               ')': 'closedBracket',
-               '.': 'dote',
-               '*': 'multiplexer',
-               '|': 'orSymbol'
-             }
+         var oneSymbolMap = {
+           '(': "openBracket",
+           ')': 'closedBracket',
+           '.': 'dote',
+           '*': 'multiplexer',
+           '|': 'orSymbol'
+         }
 
-             var oneSymbolRegex = "["
-             for (var symbol in oneSymbolMap) {
-               if (!oneSymbolMap.hasOwnProperty(symbol)) continue
+         var oneSymbolRegex = "["
+         for (var symbol in oneSymbolMap) {
+           if (!oneSymbolMap.hasOwnProperty(symbol)) continue
 
-               assert(symbol.match(/[^a-zA-Z0-9]/))
-               oneSymbolRegex += '\\' + symbol
-             }
-             oneSymbolRegex += ']'
+           assert(symbol.match(/[^a-zA-Z0-9]/))
+           oneSymbolRegex += '\\' + symbol
+         }
+         oneSymbolRegex += ']'
 
-             var RegexpFileHighlightRules = function () {
+         var RegexpFileHighlightRules = function () {
 
-               this.$rules = {
-                 "#atom": [
-                   { token: "dote",
-                     regex: /\./,
-                     next : "afterAtom"
-                   },
+           this.$rules = {
+             "#atom": [
+               { token: "dote",
+                 regex: /\./,
+                 next: "afterAtom"
+               },
 
-                   { token: 'controlLetter',
-                     regex: /\\c[a-z]/i,
-                     next : "afterAtom"
-                   },
-                   { token: 'characterClassEscape',
-                     regex: /\\[sSdDwW]/,
-                     next : "afterAtom"
-                   },
-                   { token: 'controlEscape',
-                     regex: /\\[fnrtv]/,
-                     next : "afterAtom"
-                   },
+               { token: 'controlLetter',
+                 regex: /\\c[a-z]/i,
+                 next: "afterAtom"
+               },
+               { token: 'characterClassEscape',
+                 regex: /\\[sSdDwW]/,
+                 next: "afterAtom"
+               },
+               { token: 'controlEscape',
+                 regex: /\\[fnrtv]/,
+                 next: "afterAtom"
+               },
 
-                   { token: "defText",
-                     regex: /[^\^\$\\\.\|\*\+\?\(\)\[\]\{\}\/]/,
-                     next : "afterAtom"
-                   }
-                 ],
-                 
-                 "#term": [
-                   { include: "#atom" },
-                     
-                   { token: "assertion",
-                     regex: /\^|\$|\\b|\\B/,
-                     next : "start"
-                   },
-
-                   { token: 'orSymbol',
-                     regex: /\|/,
-                     next: "start"
-                   },
-
-                   { token: 'openBracket',
-                     regex: /\((?:\?[:=!])?/,
-                     next: "start"
-                   },
-
-                   { token: 'closedBracket',
-                     regex: /\)/,
-                     next: "afterAtom"
-                   }
-                 ],
-
-                 start: [
-                   { include: "#term" },
-                     
-                   { token: 'error',
-                     regex: /[*+?]|\{\d+(?:,\d*)?\}\??/,
-                     next: "start"
-                   },
-
-                   {
-                     token: 'error',
-                     regex: /./
-                   }
-                 ],
-
-                 afterAtom: [
-                   { include: "#term" },
-
-                   { token: 'quantifier.ace_specSymbol',
-                     regex: /[*+?]|\{\d+(?:,\d*)?\}\??/,
-                     next: "start"
-                   },
-
-                   {
-                     token: 'error',
-                     regex: /./
-                   }
-                 ]
+               { token: "defText",
+                 regex: /[^\^\$\\\.\|\*\+\?\(\)\[\]\{\}\/]/,
+                 next: "afterAtom"
                }
+             ],
 
-               this.normalizeRules();
-             };
+             "#term": [
+               { include: "#atom" },
 
-             oop.inherits(RegexpFileHighlightRules, TextHighlightRules);
+               { token: "assertion",
+                 regex: /\^|\$|\\b|\\B/,
+                 next: "start"
+               },
 
-             exports.RegexpFileHighlightRules = RegexpFileHighlightRules;
-           });
+               { token: 'orSymbol',
+                 regex: /\|/,
+                 next: "start"
+               },
+
+               { token: 'openBracket',
+                 regex: /\((?:\?[:=!])?/,
+                 next: "start"
+               },
+
+               { token: 'closedBracket',
+                 regex: /\)/,
+                 next: "afterAtom"
+               }
+             ],
+
+             start: [
+               { include: "#term" },
+
+               { token: 'error',
+                 regex: /[*+?]|\{\d+(?:,\d*)?\}\??/,
+                 next: "start"
+               },
+
+               {
+                 token: 'error',
+                 regex: /./
+               }
+             ],
+
+             afterAtom: [
+               { include: "#term" },
+
+               { token: 'quantifier.ace_specSymbol',
+                 regex: /[*+?]|\{\d+(?:,\d*)?\}\??/,
+                 next: "start"
+               },
+
+               {
+                 token: 'error',
+                 regex: /./
+               }
+             ]
+           }
+
+           this.normalizeRules();
+         };
+
+         oop.inherits(RegexpFileHighlightRules, TextHighlightRules);
+
+         exports.RegexpFileHighlightRules = RegexpFileHighlightRules;
+       });
