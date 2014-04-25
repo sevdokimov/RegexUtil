130e3909d99a3ee606e04714f4fa7bf48d5fd962
Sergey Evdokimov <sergey.evdokimov@jetbrains.com>
2014-04-25 13:49:47 +0400

    Extract MatchedBracketMarket class

--- web/content/js/regexp/regexEditorApi.js
+++ web/content/js/regexp/regexEditorApi.js
@@ -69,92 +69,7 @@ function installFlagsCheckboxListener(regexpEditor, checkboxes) {
 }
 
 function installRegexpHighlighter(regexpEditor) {
-  var TokenIterator = require('ace/token_iterator').TokenIterator
-  var Range = ace.require("ace/range").Range;
-  
-  var bracketMarker = {
-    id: 'regexBracketHighlighter',
-
-    openBrackets: ['openBracket', 'charClassStart'],
-    
-    parentBracket: {
-      openBracket: 'closedBracket',
-      closedBracket: 'openBracket',
-      charClassStart: 'charClassEnd',
-      charClassEnd: 'charClassStart'
-    },
-
-    update: function (html, markerLayer, session, config) {
-      if (!regexpEditor.isFocused()) return
-      
-      var cursorPos = session.getSelection().getCursor()
-
-      var t = session.getTokenAt(cursorPos.row, cursorPos.column + 1)
-
-      var matchBracket
-      
-      if (!t || !(matchBracket = this.parentBracket[t.type])) {
-        if (cursorPos.column == 0) {
-          return
-        }
-        
-        t = session.getTokenAt(cursorPos.row, cursorPos.column)
-
-        if (!t || !(matchBracket = this.parentBracket[t.type])) {
-          return
-        }
-      }
-
-      var itr = new TokenIterator(session, cursorPos.row, t.start + 1)
-      var currentToken;
-      
-      var forward = this.openBrackets.indexOf(t.type) > -1
-      
-      var range = 0
-      while (true) {
-        if (forward) {
-          itr.stepForward()
-        }
-        else {
-          itr.stepBackward()
-        }
-
-        currentToken = itr.getCurrentToken();
-        if (!currentToken) {
-          return
-        }
-
-        if (currentToken.type == matchBracket) {
-          if (range == 0) {
-            break
-          }
-
-          range--
-        }
-        else if (currentToken.type == t.type) {
-          range++
-        }
-      }
-
-      var firstBracketRange = new Range(cursorPos.row, t.start, cursorPos.row, t.start + t.value.length)
-      markerLayer.drawSingleLineMarker(html,
-                                       firstBracketRange.toScreenRange(session),
-                                       'matchedBracket',
-                                       config);
-      
-      var matchedBracketRow = itr.getCurrentTokenRow()
-      var matchedBracketColumn = itr.getCurrentTokenColumn()
-      var secondBracketRange = new Range(matchedBracketRow, matchedBracketColumn, matchedBracketRow, matchedBracketColumn + currentToken.value.length)
-
-      markerLayer.drawSingleLineMarker(html,
-                                       secondBracketRange.toScreenRange(session),
-                                       'matchedBracket',
-                                       config);
-      
-    }
-  }
-
-  regexpEditor.getSession().addDynamicMarker(bracketMarker)
+  regexpEditor.getSession().addDynamicMarker(new MatchedBracketMarket(regexpEditor))
 
   regexpEditor.on("change", function() {
     regexpEditor.onChangeBackMarker()
@@ -171,4 +86,89 @@ function installRegexpHighlighter(regexpEditor) {
   regexpEditor.getSession().selection.on('changeCursor', function() {
     regexpEditor.onChangeBackMarker()
   })
+}
+
+function MatchedBracketMarket(regexpEditor) {
+  var TokenIterator = require('ace/token_iterator').TokenIterator
+  var Range = ace.require("ace/range").Range;
+
+  this.id = 'regexBracketHighlighter'
+
+  this.openBrackets = ['openBracket', 'charClassStart']
+
+  this.parentBracket = {
+    openBracket: 'closedBracket',
+    closedBracket: 'openBracket',
+    charClassStart: 'charClassEnd',
+    charClassEnd: 'charClassStart'
+  }
+
+  this.update = function (html, markerLayer, session, config) {
+    if (!regexpEditor.isFocused()) return
+
+    var cursorPos = session.getSelection().getCursor()
+
+    var t = session.getTokenAt(cursorPos.row, cursorPos.column + 1)
+
+    var matchBracket
+
+    if (!t || !(matchBracket = this.parentBracket[t.type])) {
+      if (cursorPos.column == 0) {
+        return
+      }
+
+      t = session.getTokenAt(cursorPos.row, cursorPos.column)
+
+      if (!t || !(matchBracket = this.parentBracket[t.type])) {
+        return
+      }
+    }
+
+    var itr = new TokenIterator(session, cursorPos.row, t.start + 1)
+    var currentToken;
+
+    var forward = this.openBrackets.indexOf(t.type) > -1
+
+    var range = 0
+    while (true) {
+      if (forward) {
+        itr.stepForward()
+      }
+      else {
+        itr.stepBackward()
+      }
+
+      currentToken = itr.getCurrentToken();
+      if (!currentToken) {
+        return
+      }
+
+      if (currentToken.type == matchBracket) {
+        if (range == 0) {
+          break
+        }
+
+        range--
+      }
+      else if (currentToken.type == t.type) {
+        range++
+      }
+    }
+
+    var firstBracketRange = new Range(cursorPos.row, t.start, cursorPos.row, t.start + t.value.length)
+    markerLayer.drawSingleLineMarker(html,
+                                     firstBracketRange.toScreenRange(session),
+                                     'matchedBracket',
+                                     config);
+
+    var matchedBracketRow = itr.getCurrentTokenRow()
+    var matchedBracketColumn = itr.getCurrentTokenColumn()
+    var secondBracketRange = new Range(matchedBracketRow, matchedBracketColumn, matchedBracketRow, matchedBracketColumn + currentToken.value.length)
+
+    markerLayer.drawSingleLineMarker(html,
+                                     secondBracketRange.toScreenRange(session),
+                                     'matchedBracket',
+                                     config);
+
+  }
 }
