8049e14101b2afd1b5a2245b6b764fe8c647fec7
Sergey Evdokimov <sergey.evdokimov@jetbrains.com>
2014-04-27 17:24:55 +0400

    Refactoring

--- web/content/js/regexp/related_elements_marker.js
+++ web/content/js/regexp/related_elements_marker.js
@@ -4,8 +4,84 @@ define('ess/regex/related_elements_marker', ['ace/token_iterator', 'ace/range'],
   var TokenIterator = require('ace/token_iterator').TokenIterator
   var Range = ace.require('ace/range').Range;
 
-  function isSupportedToken(tokenType) {
-    return tokenType == 'orSymbol'
+  function orSymbolRelatedElementFinder(itr, t, consumer) {
+    var range = 0
+    var savedItrState = saveTokenIteratorState(itr)
+
+    var lastOr = t;
+    lastOr.row = itr.getCurrentTokenRow()
+
+    do {
+      var currentToken = itr.stepBackward()
+      if (!currentToken) break
+
+      if (currentToken.type == 'openBracket') {
+        if (range == 0) {
+          break
+        }
+        else {
+          range--
+        }
+      }
+      else if (currentToken.type == 'closedBracket') {
+        range++
+      }
+      else if (range == 0 && currentToken.type == 'orSymbol') {
+        var column = itr.getCurrentTokenColumn()
+        var currentTokenRow = itr.getCurrentTokenRow();
+        currentToken.start = column
+        currentToken.row = currentTokenRow
+
+        consumer(new Range(currentTokenRow, column + currentToken.value.length, lastOr.row, lastOr.start))
+
+        lastOr = currentToken
+      }
+    } while (true)
+
+    itr.stepForward()
+
+    consumer(new Range(itr.getCurrentTokenRow(), itr.getCurrentTokenColumn(), lastOr.row, lastOr.start))
+
+    // Go forward
+    loadTokenIteratorState(itr, savedItrState)
+
+    lastOr = t;
+    range = 0
+    do {
+      currentToken = itr.stepForward()
+      if (!currentToken) break
+
+      if (currentToken.type == 'openBracket') {
+        range++
+      }
+      else if (currentToken.type == 'closedBracket') {
+        if (range == 0) {
+          break
+        }
+        else {
+          range--
+        }
+      }
+      else if (range == 0 && currentToken.type == 'orSymbol') {
+        column = itr.getCurrentTokenColumn()
+        currentTokenRow = itr.getCurrentTokenRow();
+        currentToken.start = column
+        currentToken.row = currentTokenRow
+
+        consumer(new Range(lastOr.row, lastOr.start + lastOr.value.length, currentTokenRow, column))
+
+        lastOr = currentToken
+      }
+    } while (true)
+
+    itr.stepBackward()
+
+    consumer(new Range(lastOr.row, lastOr.start + lastOr.value.length, 
+                       itr.getCurrentTokenRow(), itr.getCurrentTokenColumn() + itr.getCurrentToken().value.length))
+  } 
+  
+  var supportedRelatedElements = {
+    orSymbol: orSymbolRelatedElementFinder
   }
   
   var RelatedElementMarker = function(regexpEditor) {
@@ -16,123 +92,25 @@ define('ess/regex/related_elements_marker', ['ace/token_iterator', 'ace/range'],
 
       var t = session.getTokenAt(cursorPos.row, cursorPos.column + 1)
 
-      var matchBracket
-
-      if (!t || (!isSupportedToken(t.type))) {
+      if (!t || (!supportedRelatedElements.hasOwnProperty(t.type))) {
         if (cursorPos.column == 0) {
           return
         }
 
         t = session.getTokenAt(cursorPos.row, cursorPos.column)
 
-        if (!t || (!isSupportedToken(t.type))) {
+        if (!t || (!supportedRelatedElements.hasOwnProperty(t.type))) {
           return
         }
       }
 
       var itr = new TokenIterator(session, cursorPos.row, t.start + 1)
-      var currentToken;
-      var range = 0
 
-      if (t.type == 'orSymbol') {
-        var savedItrState = saveTokenIteratorState(itr)
-
-        var lastOr = t;
-        lastOr.row = cursorPos.row
-
-        do {
-          currentToken = itr.stepBackward()
-          if (!currentToken) break
-
-          if (currentToken.type == 'openBracket') {
-            if (range == 0) {
-              break
-            }
-            else {
-              range--
-            }
-          }
-          else if (currentToken.type == 'closedBracket') {
-            range++
-          }
-          else if (range == 0 && currentToken.type == 'orSymbol') {
-            var column = itr.getCurrentTokenColumn()
-            var currentTokenRow = itr.getCurrentTokenRow();
-            currentToken.start = column
-            currentToken.row = currentTokenRow
-
-            column += currentToken.value.length
-
-            if (column != lastOr.start || currentTokenRow != lastOr.row) {
-              drawLineMarker(markerLayer, html,
-                             new Range(itr.getCurrentTokenRow(), column, lastOr.row, lastOr.start),
-                             session, 'relatedToken', config)
-            }
-
-            lastOr = currentToken
-          }
-        } while (true)
-
-        itr.stepForward()
-
-        column = itr.getCurrentTokenColumn()
-        if (column != lastOr.start || itr.getCurrentTokenRow() != lastOr.row) {
-          drawLineMarker(markerLayer, html,
-                         new Range(itr.getCurrentTokenRow(), column, lastOr.row, lastOr.start),
-                         session, 'relatedToken', config)
+      supportedRelatedElements[t.type](itr, t, function(range) {
+        if (!range.isEmpty()) {
+          drawLineMarker(markerLayer, html, range, session, 'relatedToken', config)
         }
-
-        // Go forward
-        loadTokenIteratorState(itr, savedItrState)
-
-        lastOr = t;
-
-        do {
-          currentToken = itr.stepForward()
-          if (!currentToken) break
-
-          if (currentToken.type == 'openBracket') {
-            range++
-          }
-          else if (currentToken.type == 'closedBracket') {
-            if (range == 0) {
-              break
-            }
-            else {
-              range--
-            }
-          }
-          else if (range == 0 && currentToken.type == 'orSymbol') {
-            column = itr.getCurrentTokenColumn()
-            currentTokenRow = itr.getCurrentTokenRow();
-            currentToken.start = column
-            currentToken.row = currentTokenRow
-
-            var markerStart = lastOr.start + lastOr.value.length
-
-            if (column != markerStart || currentTokenRow != lastOr.row) {
-              drawLineMarker(markerLayer, html,
-                             new Range(lastOr.row, markerStart, currentTokenRow, column),
-                             session, 'relatedToken', config)
-            }
-
-            lastOr = currentToken
-          }
-        } while (true)
-
-        itr.stepBackward()
-
-        var markerEnd = itr.getCurrentTokenColumn() + itr.getCurrentToken().value.length
-        markerStart = lastOr.start + lastOr.value.length
-        if (markerStart != markerEnd || itr.getCurrentTokenRow() != lastOr.row) {
-          drawLineMarker(markerLayer, html,
-                         new Range(lastOr.row, markerStart, itr.getCurrentTokenRow(), markerEnd),
-                         session, 'relatedToken', config)
-        }
-      }
-      else {
-        assert(false)
-      }
+      })
     }
   };
 
