f2a06a3d5c9a24431272f4d9d4a803141a100c73
Sergey Evdokimov <sergey.evdokimov@jetbrains.com>
2014-04-29 19:14:05 +0400

    Add groups pane

--- web/content/js/regexp/editorDependencies.js
+++ web/content/js/regexp/editorDependencies.js
@@ -1,6 +1,6 @@
 var Range = ace.require("ace/range").Range;
 
-function installRegexpFindDependency(regexpEditor, textEditor, matchesResult) {
+function installRegexpFindDependency(regexpEditor, textEditor, matchesResult, groupTable) {
   var marker = {
     id: 'regexTextFindMarked',
 
@@ -18,6 +18,12 @@ function installRegexpFindDependency(regexpEditor, textEditor, matchesResult) {
       
       var matchCount = 0
       
+      var currentMatchResult
+      var currentMatchResultRange
+      var lastMatchResult
+      
+      var cursorPos = session.getSelection().getCursor()
+      
       while (r = regex.exec(text)) {
         if (r[0].length == 0) {
           break // regexp is empty
@@ -31,8 +37,17 @@ function installRegexpFindDependency(regexpEditor, textEditor, matchesResult) {
         drawLineMarker(markerLayer, html, range, session, (matchCount & 1) ? 'matched2' : 'matched1', config)
         
         matchCount++
+        lastMatchResult = r
         
-        if (!regex.global) break
+        if (!regex.global) {
+          break
+        }
+        else {
+          if (range.contains(cursorPos.row, cursorPos.column)) {
+            currentMatchResult = r
+            currentMatchResultRange = range
+          }
+        }
       }
       
       if (matchesResult) {
@@ -52,6 +67,41 @@ function installRegexpFindDependency(regexpEditor, textEditor, matchesResult) {
         
         matchesResult.text(matchResultText)
       }
+      
+      if (groupTable) {
+        if (matchCount == 0) {
+          $('td:last-child', groupTable).html("<span class='spec'>No matches found<span>")
+        }
+        else {
+          if (matchCount == 1) {
+            currentMatchResult = lastMatchResult
+            currentMatchResultRange = range
+          }
+
+          if (currentMatchResultRange) {
+            drawLineMarker(markerLayer, html, currentMatchResultRange, session, 'currentMatchResult', config)
+          }
+
+          if (currentMatchResult) {
+            var groupRows = groupTable.groupRows
+            if (groupRows) {
+              for (var i = 0; i < groupRows.length; i++) {
+                var tr = groupRows[i]
+
+                var s = ""
+                if (currentMatchResult) {
+                  s = currentMatchResult[i] || ""
+                }
+                $('td:last-child', tr).empty().append($("<span class='groupText'></span>").text(s))
+              }
+            }
+          }
+          else {
+            $('td:last-child', groupTable).html("<span class='spec'>please, move the cursor to matched text<span>")
+          }
+          
+        }
+      }
     }
   }
 
@@ -60,10 +110,43 @@ function installRegexpFindDependency(regexpEditor, textEditor, matchesResult) {
   textEditor.on("change", function() {
     textEditor.onChangeBackMarker()
   })
-
-  regexpEditor.addRegexChangeListener(function () {
+  textEditor.getSession().selection.on('changeCursor', function() {
     textEditor.onChangeBackMarker()
   })
+
+  function fixGroupCount() {
+    if (groupTable) {
+      var bracketStructure = regexpEditor.session.bracketStructure
+
+      var groupRows = groupTable.groupRows
+      if (groupRows == undefined) {
+        groupTable.groupRows = groupRows = []
+        groupRows.push($("tr", groupTable)[0])
+      }
+      
+      if (groupRows.length != bracketStructure.groups.length + 1) {
+        if (groupRows.length > bracketStructure.groups.length + 1) {
+          while (groupRows.length > bracketStructure.groups.length + 1) {
+            $(groupRows.pop()).remove()
+          }
+        }
+        else {
+          for (var i = groupRows.length; i < bracketStructure.groups.length + 1; i++) {
+            var e = $("<tr><td>#" + i + "</td><td></td></tr>")
+            groupTable.append(e)
+            groupRows.push(e)
+          }
+        }
+      }
+    }
+  }
+  
+  regexpEditor.addRegexChangeListener(function () {
+    textEditor.onChangeBackMarker()
+    fixGroupCount()
+  })
+
+  fixGroupCount()
 
   //regexpEditor.getSession().selection.on("changeSelection", function () {
   //  if (!regexpEditor.curOp.command.name) {
