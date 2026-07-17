4a2eefe4e488c962115c9d884b2e367d24de9878
Sergey Evdokimov <sergey.evdokimov@behavox.com>
2026-07-17 20:16:53 +0300

    Fix a compilation error: add `junit:junit:4.13.2` dependency.

--- RegexpTester/build.gradle.kts
+++ RegexpTester/build.gradle.kts
@@ -1,3 +1,6 @@
+import org.gradle.kotlin.dsl.invoke
+import org.gradle.kotlin.dsl.test
+
 plugins {
     id("java")
     id("org.jetbrains.intellij.platform") version "2.18.1"
@@ -17,21 +20,22 @@ repositories {
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin.html
 dependencies {
     intellijPlatform {
-        intellijIdea("2026.2")
+        intellijIdea("2025.3.1")
         testFramework(org.jetbrains.intellij.platform.gradle.TestFrameworkType.Platform)
 
         // Add necessary plugin dependencies for compilation here, example:
         // bundledPlugin("com.intellij.java")
     }
 
-    testImplementation("org.assertj:assertj-core:3.17.2")
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.assertj:assertj-core:3.27.3")
     testImplementation("org.mockito:mockito-core:4.4.0")
 }
 
 intellijPlatform {
     pluginConfiguration {
         ideaVersion {
-            sinceBuild = "262"
+            sinceBuild = "253"
         }
 
         changeNotes = """
@@ -55,6 +59,10 @@ tasks {
     withType<JavaCompile> {
         sourceCompatibility = "21"
         targetCompatibility = "21"
+    }
+
+    test {
+        useJUnit()
     }
 }
 
